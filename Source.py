9c63c87675180c784a8e4138870a8d181e182474
pastrelic <patrykwyzgowskig9@wp.pl>
2018-05-22 21:59:53 +0200

    Added a file containing a definition for prototypical nested_to_dict function

--- Source.py
+++ Source.py
@@ -1,14 +1,31 @@
 import requests
 import json
-from nestedToDict import nested_to_dict
+#from nestedToDict import nested_to_dict
 import csv
 
+def nested_to_dict(json):
+    flattened_to_dict = {}
+
+    def flatten(to_be_flattened, col_name=''):
+        if type(to_be_flattened) is dict:
+            for key in to_be_flattened:
+                flatten(to_be_flattened[key], col_name + key + '_')
+        elif type(to_be_flattened) is list:
+            counter = 0
+            for key in to_be_flattened:
+                flatten(key, col_name + str(counter) + '_')
+                counter=counter+1
+
+        else:
+            flattened_to_dict[col_name[:-1]] = to_be_flattened
+
+    flatten(json)
+    return flattened_to_dict
 URL = "https://api.spacexdata.com/v2/launches"
 r = requests.get(url=URL)
 
-
-json_launches = r.json()
 launches_parsed = json.loads(r.text)
 
-print(nested_to_dict(json_launches))
-print(launches_parsed)
+newer = [nested_to_dict(index) for index in launches_parsed]
+
+print(newer[0:1])
