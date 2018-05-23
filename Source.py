3664b8e129a9d8b0d82901d7111d43f7b71bab93
pastrelic <pastrelict909@gmail.com>
2018-05-23 22:33:26 +0200

    Added additional columns to data frame, optimized droping

--- Source.py
+++ Source.py
@@ -6,20 +6,20 @@ from collections import OrderedDict
 def nested_to_dict(json):
     flattened_to_dict = {}
 
-    def flatten(to_be_flattened, col_name=''):
+    def decompose(to_be_flattened, col_name=''):
         if type(to_be_flattened) is dict:
             for key in to_be_flattened:
-                flatten(to_be_flattened[key], col_name + key + '_')
+                decompose(to_be_flattened[key], col_name + key + '_')
         elif type(to_be_flattened) is list:
             counter = 0
             for key in to_be_flattened:
-                flatten(key, col_name + str(counter) + '_')
+                decompose(key, col_name + str(counter) + '_')
                 counter=counter+1
 
         else:
             flattened_to_dict[col_name[:-1]] = to_be_flattened
 
-    flatten(json)
+    decompose(json)
     return flattened_to_dict
 URL = "https://api.spacexdata.com/v2/launches"
 r = requests.get(url=URL)
@@ -34,8 +34,11 @@ fajnal = [OrderedDict(dic) for dic in newer]
 df_final = pd.DataFrame.from_records(fajnal,index='flight_number')
 df_final.to_csv('df_final.csv')
 
-masses = pd.DataFrame(columns=['total payload mass'])
-masses['total payload mass'] = df_final['rocket_second_stage_payloads_0_payload_mass_kg'].fillna(0) + df_final['rocket_second_stage_payloads_1_payload_mass_kg'].fillna(0)
-masses = masses[(masses.T != 0).any()]
+masses = pd.DataFrame(columns=['launch_year'])
+masses['launch_year'] = df_final['launch_year']
+masses['total_payload_mass'] = df_final['rocket_second_stage_payloads_0_payload_mass_kg'].fillna(0) + df_final['rocket_second_stage_payloads_1_payload_mass_kg'].fillna(0)
+masses['mass_returned'] = df_final['rocket_second_stage_payloads_0_mass_returned_kg'].fillna(0)
+#masses = masses[(masses.T != 0).any()]
+masses.drop(masses[masses['total_payload_mass'] == 0].index, inplace=True)
 
-#print(masses)
+print(masses)
